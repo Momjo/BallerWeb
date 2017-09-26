c4c51eddc2ed9fc331ae1a17a62292ffda9b33bc
MOJO <jafari.mohammad478@gmail.com>
2017-09-26 23:54:17 +0200

    Update urls.py

--- ballerapp/urls.py
+++ ballerapp/urls.py
@@ -9,12 +9,10 @@ from ballerapp.views import AdresseListView
 app_nam= 'ballerapp'
 
 urlpatterns = (
-
+        
         url(r'^register/$', views.register_view, name='register'),
         url(r'^home/$', AdresseListView.as_view(), name='adresse-list-view'),
         url(r'^login/$', views.login_view, name='login'),
         url(r'^add_place/$', views.add_new_place, name='add_new_place'),
         url(r'^logout/$', views.logout_view, name='login'),
-
-
 )
