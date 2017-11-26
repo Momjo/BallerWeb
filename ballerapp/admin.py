328ce28e33433ad224e08ffffc973dc0044bcea4
Momjo <jafari.mohammad478@gmail.com>
2017-11-26 15:16:17 +0100

    new-update

--- ballerapp/admin.py
+++ ballerapp/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Adresse, UserProfile, Profile
+from .models import Adresse# UserProfile, Profile
 
 class AddresseAdmin(admin.ModelAdmin):
     list_display = ('country', 'city', 'street', 'hous_number' )
 	
 
 admin.site.register(Adresse, AddresseAdmin)
-admin.site.register(UserProfile)
-admin.site.register(Profile)
+#admin.site.register(UserProfile)
+#admin.site.register(Profile)
--- ballerapp/forms.py
+++ ballerapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from ballerapp.models import UserProfile, Adresse
+from ballerapp.models import  Adresse #UserProfile,
 from django.contrib.auth import authenticate, login, logout, get_user_model
 
 User = get_user_model()
@@ -13,16 +13,16 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'email', 'password')
 
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ('website', 'picture')
+# class UserProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = UserProfile
+#         fields = ('website', 'picture')
 
 
 class AddNewPlace(forms.ModelForm):
     class Meta:
         model = Adresse
-        fields = ('country', 'city', 'street', 'hous_number', 'place_pic')
+        fields = ('country', 'city', 'street', 'hous_number', 'picture')
 
 
 
