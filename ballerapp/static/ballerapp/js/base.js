8cffed27842753ec0892bd86ddb7391c062ecf0c
MOJO <jafari.mohammad478@gmail.com>
2018-01-11 15:01:42 +0100

    Updating base design

--- ballerapp/static/ballerapp/js/base.js
+++ ballerapp/static/ballerapp/js/base.js
@@ -2,4 +2,9 @@ function showImage(url) {
   $("#myModal").modal('show');
   $("img.picture-modal-img").attr('src', url);
 };
+function maximize(url){
+   $("#myModal").modal('show');
+  $("img.picture-modal-img").attr('src', url);
+
+}
 
